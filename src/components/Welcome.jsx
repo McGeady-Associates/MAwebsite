a4b6c692b74298c4171954574ea8b63c4917f05f
Tony Xu <xuology@gmail.com>
2017-11-03 17:08:38 -0400

    add welcome text as static asset, need to conver it to some other format. Maybe md

--- src/components/Welcome.jsx
+++ src/components/Welcome.jsx
@@ -1,12 +1,30 @@
 import React from 'react';
+import welcomeText from '../../static/text/welcome';
 
 const Welcome = (props) => {
   return (
     <section id="welcomeContainer"
       className="bg-primary text-white text-center"
-      style={{backgroundColor: '#b4c889', height: 500}}
+      style={{backgroundColor: '#b4c889', height: '30%'}}
     >
+    <h1>
       WELCOME TO MCGEADY & ASSOCIATES
+    </h1>
+    <p>
+      {welcomeText.p1}
+    </p>
+    <p>
+      {welcomeText.p2}
+    </p>
+    <p>
+      {welcomeText.p3}
+    </p>
+    <p>
+      {welcomeText.p4}
+    </p>
+    <p>
+      {welcomeText.p5}
+    </p>
     </section>
   )
 
---
+++ static/text/welcome.js
@@ -0,0 +1,13 @@
+module.exports = {
+  p1: `McGeady & Associates is a full-service corporate meeting, conference management, and special event planning company, serving clients nationwide. We have designed and managed over 400 national and regional meetings, retreats, conferences, and special events, ranging in size from 25 to 1,500 persons, and are experienced in budget planning, program development, vendor relations, meeting logistics and event production.  Founder and President Patrice McGeady formed the entrepreneurial woman-owned company in 2000.  McGeady & Associates is headquartered in Maryland.`,
+
+  p2: `We believe in partnering with you, always focusing on the strategic goals underlying any successful event or initiative.Our ability to seamlessly integrate as a member of your team allows us to assist in expertly managing your event, assuring your participants walk away with a“ best in class” experience while maximizing your event budget and resources.`,
+
+
+  p3: `McGeady & Associates approaches each event and initiative as an individual challenge to bring people together for a rewarding experience. We work behind the scenes, using all of our experience and resources to make sure each and every detail is covered thoroughly.`,
+
+
+  p4: `We measure success by the lasting, positive impact our events leave with participants, sponsors, and most importantly, our clients.`,
+  
+  p5: `Contact us today to find out how we can assist in achieving your event planning needs.`
+};
