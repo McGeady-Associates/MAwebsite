b40ca6708ca16c118a9cf741d1db778cb7ed2617
Tony Xu <xuology@gmail.com>
2017-11-03 16:38:02 -0400

    add logo to header

--- src/layouts/index.js
+++ src/layouts/index.js
@@ -2,24 +2,22 @@ import React from 'react'
 import PropTypes from 'prop-types'
 import Link from 'gatsby-link'
 import Helmet from 'react-helmet'
+import logo from '../../static/img/logo.jpg'
 
 import './index.css'
 
 const Header = () => (
   <div
     style={{
-      background: 'rebeccapurple',
-      marginBottom: '1.45rem',
+      background: 'white',
     }}
   >
     <div
       style={{
-        margin: '0 auto',
-        maxWidth: 960,
-        padding: '1.45rem 1.0875rem',
+        margin: '0',
       }}
     >
-      <h1 style={{ margin: 0 }}>
+      <h1 style={{ margin: 0}}>
         <Link
           to="/"
           style={{
@@ -27,7 +25,7 @@ const Header = () => (
             textDecoration: 'none',
           }}
         >
-          Gatsby
+          <img src={logo} height="100" width="350"/>
         </Link>
       </h1>
     </div>
@@ -47,7 +45,6 @@ const TemplateWrapper = ({ children }) => (
     <div
       style={{
         margin: '0 auto',
-        maxWidth: 960,
         padding: '0px 1.0875rem 1.45rem',
         paddingTop: 0,
       }}
