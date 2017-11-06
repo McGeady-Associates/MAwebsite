fcd2bbe13f531425b9164339f2827a78a12a381f
Tony Xu <xuology@gmail.com>
2017-11-05 21:16:56 -0500

    add logo and add anchor tags, need to fix styling

--- src/layouts/index.js
+++ src/layouts/index.js
@@ -21,26 +21,15 @@ const Header = () => (
     >
       <Navbar>
         <Navbar.Header>
-      <Navbar.Brand>
-        <a href="#">React-Bootstrap</a>
-      </Navbar.Brand>
+          <a href="#"><img src={logo} style={{width: '50%'}}/></a>
       <Navbar.Toggle />
     </Navbar.Header>
     <Navbar.Collapse>
       <Nav>
-        <NavItem eventKey={1} href="#">Link</NavItem>
-        <NavItem eventKey={2} href="#">Link</NavItem>
-        <NavDropdown eventKey={3} title="Dropdown" id="basic-nav-dropdown">
-          <MenuItem eventKey={3.1}>Action</MenuItem>
-          <MenuItem eventKey={3.2}>Another action</MenuItem>
-          <MenuItem eventKey={3.3}>Something else here</MenuItem>
-          <MenuItem divider />
-          <MenuItem eventKey={3.3}>Separated link</MenuItem>
-        </NavDropdown>
-      </Nav>
-      <Nav pullRight>
-        <NavItem eventKey={1} href="#">Link Right</NavItem>
-        <NavItem eventKey={2} href="#">Link Right</NavItem>
+        <NavItem eventKey={1} href="#">About Us</NavItem>
+        <NavItem eventKey={2} href="#">Services</NavItem>
+        <NavItem eventKey={3} href="#">Key Client Engagements</NavItem>
+        <NavItem eventKey={4} href="#">Contact Us</NavItem>
       </Nav>
     </Navbar.Collapse>
       </Navbar>
