e327818754e1fa3691660ea8692078a2939fd1bd
Tony Xu <xuology@gmail.com>
2017-11-08 22:57:23 -0500

    #3 use react bootstrap table to box client engagements. Might end up using carousel

--- src/components/ClientProjects.jsx
+++ src/components/ClientProjects.jsx
@@ -8,19 +8,19 @@ const ClientProjects = (props) => {
       className="text-white text-center"
       style={{height: '30%'}}
     >
-    <h1 style={{display: 'flex', justifyContent: 'center'}}>
-      Key Client Engagements
-    </h1>
-    <div style={{display: 'flex', flex: '0 0 auto'}}>
-      <ClientProjectsItem title={clientProjectsText.egsk.title} details={clientProjectsText.egsk.details} />
-      <ClientProjectsItem title={clientProjectsText.nawl.title} details={clientProjectsText.nawl.details} />
-      <ClientProjectsItem title={clientProjectsText.mcca.title} details={clientProjectsText.mcca.details} />
-    </div>
-    <div style={{display: 'flex'}}>
-      <ClientProjectsItem title={clientProjectsText.neaf.title} details={clientProjectsText.neaf.details} />
-      <ClientProjectsItem title={clientProjectsText.aacedc.title} details={clientProjectsText.aacedc.details} />
-      <ClientProjectsItem title={clientProjectsText.ee.title} details={clientProjectsText.ee.details} />
-    </div>
+      <h1 style={{display: 'flex', justifyContent: 'center'}}>
+        Key Client Engagements
+      </h1>
+      <div style={{display: 'flex', flex: '0 0 auto'}}>
+        <ClientProjectsItem title={clientProjectsText.egsk.title} details={clientProjectsText.egsk.details} />
+        <ClientProjectsItem title={clientProjectsText.nawl.title} details={clientProjectsText.nawl.details} />
+        <ClientProjectsItem title={clientProjectsText.mcca.title} details={clientProjectsText.mcca.details} />
+      </div>
+      <div style={{display: 'flex'}}>
+        <ClientProjectsItem title={clientProjectsText.neaf.title} details={clientProjectsText.neaf.details} />
+        <ClientProjectsItem title={clientProjectsText.aacedc.title} details={clientProjectsText.aacedc.details} />
+        <ClientProjectsItem title={clientProjectsText.ee.title} details={clientProjectsText.ee.details} />
+      </div>
     </section>
   )
 }
--- src/components/ClientProjectsItem.jsx
+++ src/components/ClientProjectsItem.jsx
@@ -1,16 +1,30 @@
 import React from 'react';
+import { Table } from 'react-bootstrap';
 
 const ClientProjectsItem = (props) => {
   return (
-    <div>
-      <div style={{display: 'flex', justifyContent: 'center'}}>
-        <p><strong>{props.title}</strong></p>
-      </div>
-      <div style={{display: 'flex', flex: '3', justifyContent: 'center', alignItems: 'center'}}>
-        <p>{props.details}</p>
-      </div>
-    </div>
+    <Table responsive bordered>
+      <thead>
+        <tr>
+          <th>{props.title}</th>
+        </tr>
+      </thead>
+      <tbody>
+        <tr>
+          <td>{props.details}</td>
+        </tr>
+      </tbody>
+    </Table>
   );
 }
 
 export default ClientProjectsItem;
+
+// <div>
+//   <div style={{display: 'flex', justifyContent: 'center'}}>
+//     <p><strong>{props.title}</strong></p>
+//   </div>
+//   <div style={{display: 'flex', flex: '3', justifyContent: 'center', alignItems: 'center'}}>
+//     <p>{props.details}</p>
+//   </div>
+// </div>
