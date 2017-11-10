d8a5bfd0cdbb15d1ce425e65b8e406d7456bfa24
Tony Xu <xuology@gmail.com>
2017-11-10 11:19:50 -0500

    "#9 add image placeholder and caption to services, awaiting feedback"

--- src/components/Services.jsx
+++ src/components/Services.jsx
@@ -1,14 +1,15 @@
 import React from 'react';
 import { ListGroup, ListGroupItem } from 'react-bootstrap';
 import ServicesText from '../../static/text/servicesText';
+import imagePlcaeholder from '../../static/img/wireframe-image-placeholder.png';
 
 const Services = (props) => {
   return (
     <section id="ServicesContainer"
-      className="text-white text-center"
-      style={{backgroundColor: 'rgba(57,68,6, 1)', height: '30%'}}
+      className="text-center"
+      style={{backgroundColor: 'rgba(250, 250, 250, 1)', height: '30%'}}
     >
-    <h1 style={{display: 'flex', justifyContent: 'center'}}>
+    <h1 style={{display: 'flex', justifyContent: 'center', color: '#95B100'}}>
       McGeady & Associates Services
     </h1>
     <div style={{display: 'flex'}}>
@@ -19,7 +20,7 @@ const Services = (props) => {
               return (
                 <ListGroupItem
                   key={index}
-                  style={{backgroundColor: 'transparent', textAlign: 'left'}}
+                  style={{textAlign: 'left', color: '#95B100'}}
                 >
                   {service}
                 </ListGroupItem>
@@ -28,7 +29,9 @@ const Services = (props) => {
           }
         </ListGroup>
       </div>
-      <div style={{flex: 8}}>
+      <div style={{display: 'flex', flexDirection: 'column', flex: 8, justifyContent: 'center', alignContent: 'center'}}>
+        <img src={imagePlcaeholder} style={{margin: 80, minHeight: 400}}/>
+        <p style={{color: '#f00'}}>Maybe have an image for each service and a small caption/description so people can read more about each service?</p>
       </div>
     </div>
     </section>
